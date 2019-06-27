e489c13b91b0ee43f13e2b344ee51289e3d5fd9c
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-27 17:50:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target" );
+}
+
+function increaseRankBy(n){
+  var rank = document.querySelectorAll(".ranked-list");
+  var result=0;
+  for(var i = 0; i<rank.length;i++){
+    result=parseInt(rank[i]) + n;
+    rank[i].innerHTML = String(result);
+  }
+  
+}
+
+function deepestChild(){
+  var nested = document.querySelector("div#grand-node div div div div");
+ 
+      return nested;
+
+  
+}
